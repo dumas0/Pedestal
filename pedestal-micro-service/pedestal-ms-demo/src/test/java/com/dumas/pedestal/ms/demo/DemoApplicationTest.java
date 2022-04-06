761f0278a8f936510e4fadfb60dadd6f204bbab9
dumas <dz.ma@shunwang.com>
2022-04-06 10:03:20 +0800

    sync github code

--- pedestal-micro-service/pedestal-ms-demo/src/test/java/com/dumas/pedestal/ms/demo/DemoApplicationTest.java
+++ pedestal-micro-service/pedestal-ms-demo/src/test/java/com/dumas/pedestal/ms/demo/DemoApplicationTest.java
@@ -1,9 +1,19 @@
 package com.dumas.pedestal.ms.demo;
 
+import com.dumas.pedestal.ms.demo.web.DemoController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author dumas
@@ -13,8 +23,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTest {
 
-    @Test
-    public void test(){
+    private MockMvc mockMvc;
 
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new DemoController()).build();
+    }
+
+    @Test
+    public void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
     }
 }
